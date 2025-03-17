150dc5818e28b3c9e32afaef8671499b496c723e
Luke Dinsdale <106984091+nocturnal-luke@users.noreply.github.com>
2025-03-17 11:32:37 +0000

    Update FFT.cpp
    Supporting the class restructuring

--- FFT.cpp
+++ FFT.cpp
@@ -1,43 +1,77 @@
 #include <iostream>
+#include "CFileHandling.cpp"
+#include "CUserInput.cpp"
 #include "CFFT.cpp"
+#include "CSignalProcessor.cpp"
 
 using namespace std;
 
 int main(){
 
-    FFT temp; // create an FFT object
+    // create an instance of all neccesary class objects:
+    FFT fft; 
+    fileHandler fileHandling; 
+    userInput input; 
+    signalProcessor signal; 
 
-    temp.getFile(); // get file to process
+    double* rawData; // raw data
+    int fileLen, paddedLen; // raw & padded file length
+    double f_s, k; // sampling frequency and windowing parameter
+    // padded array, spectra, and time domain records:
+    double** paddedData,** accSpectrum, ** velSpectrum, ** dispSpectrum, ** velTime, ** dispTime;
 
-    temp.zeroPadding(); // creates padded array and allocates memory for output array
-    temp.getUserParameters(); // request parameters for computations
-    temp.computeFFT(temp.padData, temp.fileLen, -1); // compute the forward FFT
+    fileHandling.getFile(); // get file to process and read in data
+    rawData = fileHandling.getRawData(); // to pull raw data to pass to FFT
+    fileLen = fileHandling.getFileLen(); // pull file length
 
-    temp.integrate(); // to compute velocity and displacement spectra
+    input.userParameters(); // request parameters from the user
+    f_s = input.getfs(); // pull sampling frequency
+    k = input.getk(); // pull windwing parameter
 
-    temp.velDispMemory(); //allocate memory for vel/ disp time domain outputs
+    fft.setFileLen(fileLen); // to set fileLen for FFT routine
+    fft.zeroPadding(rawData, fileLen); // creates padded array and allocates memory for output array
 
-    temp.computeFFT(temp.velocitySpectra, temp.fileLen, 1); // inv transform to obtain velocity-time
-    temp.computeFFT(temp.displacementSpectra, temp.fileLen, 1); // inv transform to obtain displacement-time
+    paddedData = fft.getAcc(); // pull padded array
+    paddedLen = fft.getPaddedLen(); // pull updated array length
 
+    signal.setFileLen(paddedLen); // set filelen in SP class
+    fileHandling.setFileLen(paddedLen); // set fileLen in FH class
+
+    fft.computeFFT(paddedData, paddedLen, -1); // compute the forward FFT
+    accSpectrum = fft.getAcc(); // pull acc spectrum
+    
+    signal.setAcc(accSpectrum); // set the acceleration spectrum in SP class
+    signal.integrate(f_s); // to compute velocity and displacement spectra
+    signal.timeDomainMemory(); //allocate memory for vel/ disp time domain outputs
+    
+    // pull each of the remaining spectra and the time domain outputs
+    velSpectrum = signal.getVel();
+    dispSpectrum = signal.getDisp();
+    velTime = signal.getVelTime();
+    dispTime = signal.getDispTime();
+    
+
+    fft.computeFFT(velSpectrum, paddedLen, 1); // inv transform to obtain velocity-time
+    fft.computeFFT(dispSpectrum, paddedLen, 1); // inv transform to obtain displacement-time
+    
     // calculate magnitudes for the forward and inverse transforms
-    double** accelerationSpectra = temp.computeAmplitudes(temp.padData, -1); // magnitudes for acceleration
-    double** velocitySpectra = temp.computeAmplitudes(temp.velocitySpectra, -1); // magnitudes for velocity
-    double** displacementSpectra = temp.computeAmplitudes(temp.displacementSpectra, -1); // magnitudes for displacement
+    signal.computeAmplitudes(accSpectrum, f_s, -1); // magnitudes for acceleration
+    signal.computeAmplitudes(velSpectrum, f_s, -1); // magnitudes for velocity
+    signal.computeAmplitudes(dispSpectrum, f_s, -1); // magnitudes for displacement
 
-    double** velocityTime = temp.computeAmplitudes(temp.velocityTime, 1); // magnitudes for vel-time
-    double** displacementTime = temp.computeAmplitudes(temp.displacementTime, 1); // magnitudes for disp-time
+    signal.computeAmplitudes(velTime, f_s, 1); // magnitudes for vel-time
+    signal.computeAmplitudes(dispTime, f_s, 1); // magnitudes for disp-time
 
-    temp.hannWindow(); // window to reduce edge effects 
+    signal.hannWindow(k); // window to reduce edge effects 
 
     // write Fourier domain features
-    temp.writeFile(accelerationSpectra, "acc_spectrum"); // write magnitudes
-    temp.writeFile(velocitySpectra, "vel_spectrum");
-    temp.writeFile(displacementSpectra, "disp_spectrum");
+    fileHandling.writeFile(accSpectrum, "acc_spectrum"); // write magnitudes
+    fileHandling.writeFile(velSpectrum, "vel_spectrum");
+    fileHandling.writeFile(dispSpectrum, "disp_spectrum");
 
     // write time domain features
-    temp.writeFile(velocityTime, "velocity_time");
-    temp.writeFile(displacementTime, "displacement_time");
+    fileHandling.writeFile(velTime, "velocity_time");
+    fileHandling.writeFile(dispTime, "displacement_time");
 
     return 0;
 }

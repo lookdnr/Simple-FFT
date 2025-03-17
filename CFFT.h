f5a5641e86667487c67061c2537012985ec84723
Luke Dinsdale <106984091+nocturnal-luke@users.noreply.github.com>
2025-03-17 11:30:36 +0000

    Update CFFT.h

--- CFFT.h
+++ CFFT.h
@@ -11,45 +11,22 @@ class FFT {
 
     public:
 
-        int fileLen; // to store num lines
-        double** padData; // to store padded data (& eventually accelerationSpectra)
-        double** velocitySpectra; // to store velocity spectrum
-        double** displacementSpectra; // to store displacement spectra
-        double** velocityTime; // to store velocity in time domain
-        double** displacementTime; // to store displacement in time domain
-
         FFT(); // constructor to pull file
         FFT(const FFT &in); // copy constructor
         ~FFT(); // destructor to save file and de-allocate
-        
-        // main data movement functions
-        void getFile(); // to request file
-        void getFilenames(); // to list file names
-        bool readFile(); // read file, return 
-        void writeFile(double** accelerationSpectra, string outputName); // to write to file
-        void getUserParameters();  // to obtain FFT, clipping, and filtering params
 
         // main FFT functions
-        void allocateMemory(); // to allocate memory
-        void zeroPadding(); // to pad accelerogram for FFT
-        void paddedMemory(int nextPow); // to allocate memory for padded array
+        void zeroPadding(double* data, int arrayLen); // to pad accelerogram for FFT
+        void paddedMemory(double* data, int paddedLen); // to allocate memory for padded array
         void computeFFT(double** input, int len, int dirFlag); // to execute FFT. -1 = fwd, 1 = inverse.
-        double** computeAmplitudes(double** array, int dirFlag); // calculate magnitude of each freq component
-        void integrate(); // to perform integration in Fourier domain to get vel and disp spectra
-        void hannWindow(); // to window the data to prevent spectral leakage
 
-        // to compute velocity and displacement spectra
-        void diff(); // to perform numerical differentiation 
-        void velDispMemory(); // to allocate memory for vel and disp (time)
-        void filter(); // to filter
+        int getPaddedLen(); // getter for updated array length
+        double** getAcc(); // getter for acc spectrum
+        void setFileLen(int input); // setter for file len
 
     private:
+        int fileLen; // to store num lines
+        // to store padded data (which becomes the acc spectra) and other spectra
+        double** padData,** velocitySpectrum,** displacementSpectrum; 
 
-        string filename; // for user input filename
-        const string path = fs::current_path().string();
-        double* initData; // for storing data pulled from file
-        double f_s; // sampling frequency from user
-        double k; // Hann window constant
-        
 };
-
